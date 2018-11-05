2ec764144da2e1baf2df825c0d2564ddaf7828eb
Vinicius BF <vini91bf@gmail.com>
2018-11-05 01:25:25 -0300

    initial version

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
+
+const port = process.env.PORT || 3000
+
+app.get('/', async(request, response) => {
+    response.render('index')
+})
+
+app.use('/categorias', categorias)
+app.use('/publicacoes', publicacoes)
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Error')
+    } else {
+        console.log('Como-Fazer Server is running on port:', port)
+    }
+})
